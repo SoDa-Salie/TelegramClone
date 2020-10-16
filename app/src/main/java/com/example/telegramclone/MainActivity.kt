ac270cdcb054f86555dfb1530f31a51191e7a9ef
Nikita Mager <nickita.mager@yandex.ru>
2020-10-16 22:34:32 +0700

    3.Fill the drawer, add new colors and one style

--- app/src/main/java/com/example/telegramclone/MainActivity.kt
+++ app/src/main/java/com/example/telegramclone/MainActivity.kt
@@ -2,14 +2,18 @@ package com.example.telegramclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 import com.example.telegramclone.databinding.ActivityMainBinding
 import com.mikepenz.materialdrawer.AccountHeader
 import com.mikepenz.materialdrawer.AccountHeaderBuilder
 import com.mikepenz.materialdrawer.Drawer
 import com.mikepenz.materialdrawer.DrawerBuilder
+import com.mikepenz.materialdrawer.model.DividerDrawerItem
 import com.mikepenz.materialdrawer.model.PrimaryDrawerItem
 import com.mikepenz.materialdrawer.model.ProfileDrawerItem
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem
 
 class MainActivity : AppCompatActivity() {
 
@@ -46,11 +50,75 @@ class MainActivity : AppCompatActivity() {
             .withSelectedItem(-1)
             .withAccountHeader(mHeader)
             .addDrawerItems(
+
                 PrimaryDrawerItem().withIdentifier(100)
                     .withIconTintingEnabled(true)
-                    .withName("Создать группу")
+                    .withName(R.string.create_group)
                     .withSelectable(false)
-            ).build()
+                    .withIcon(R.drawable.ic_menu_create_groups),
+
+                PrimaryDrawerItem().withIdentifier(101)
+                    .withIconTintingEnabled(true)
+                    .withName(R.string.create_secret_chat)
+                    .withSelectable(false)
+                    .withIcon(R.drawable.ic_menu_secret_chat),
+
+                PrimaryDrawerItem().withIdentifier(102)
+                    .withIconTintingEnabled(true)
+                    .withName(R.string.create_channel)
+                    .withSelectable(false)
+                    .withIcon(R.drawable.ic_menu_create_channel),
+
+                PrimaryDrawerItem().withIdentifier(103)
+                    .withIconTintingEnabled(true)
+                    .withName(R.string.contacts)
+                    .withSelectable(false)
+                    .withIcon(R.drawable.ic_menu_contacts),
+
+                PrimaryDrawerItem().withIdentifier(104)
+                    .withIconTintingEnabled(true)
+                    .withName(R.string.phone)
+                    .withSelectable(false)
+                    .withIcon(R.drawable.ic_menu_phone),
+
+                PrimaryDrawerItem().withIdentifier(105)
+                    .withIconTintingEnabled(true)
+                    .withName(R.string.favorites)
+                    .withSelectable(false)
+                    .withIcon(R.drawable.ic_menu_favorites),
+
+                PrimaryDrawerItem().withIdentifier(106)
+                    .withIconTintingEnabled(true)
+                    .withName(R.string.settings)
+                    .withSelectable(false)
+                    .withIcon(R.drawable.ic_menu_settings),
+
+                PrimaryDrawerItem().withIdentifier(107)
+                    .withIconTintingEnabled(true)
+                    .withName(R.string.invate)
+                    .withSelectable(false)
+                    .withIcon(R.drawable.ic_menu_invate),
+
+                DividerDrawerItem(),
+
+                PrimaryDrawerItem().withIdentifier(108)
+                    .withIconTintingEnabled(true)
+                    .withName(R.string.faq)
+                    .withSelectable(false)
+                    .withIcon(R.drawable.ic_menu_help)
+
+            )
+            .withOnDrawerItemClickListener(object : Drawer.OnDrawerItemClickListener {
+                override fun onItemClick(
+                    view: View?,
+                    position: Int,
+                    drawerItem: IDrawerItem<*>
+                ): Boolean {
+                    Toast.makeText(applicationContext, position.toString(), Toast.LENGTH_SHORT).show()
+                    return false
+                }
+            })
+            .build()
 
     }
 
@@ -60,7 +128,7 @@ class MainActivity : AppCompatActivity() {
             .withHeaderBackground(R.drawable.header)
             .addProfiles(
                 ProfileDrawerItem()
-                    .withName("Yura Petrov")
+                    .withName("Nikita Mager")
                     .withEmail("name@gmail.com")
             ).build()
     }
