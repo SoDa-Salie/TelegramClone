dcfbbd59ea785c322bed79f7f0357587374ef734
Nikita Mager <nickita.mager@yandex.ru>
2020-11-17 22:25:32 +0700

    28.Gettin' permission to read contacts

--- app/src/main/java/com/example/telegramclone/MainActivity.kt
+++ app/src/main/java/com/example/telegramclone/MainActivity.kt
@@ -1,19 +1,16 @@
 package com.example.telegramclone
 
-import android.content.Context
-import android.content.Intent
+
+import android.content.pm.PackageManager
 import android.os.Bundle
-import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
 import com.example.telegramclone.activities.RegisterActivity
 import com.example.telegramclone.databinding.ActivityMainBinding
-import com.example.telegramclone.models.User
 import com.example.telegramclone.ui.fragments.ChatsFragment
 import com.example.telegramclone.ui.objects.AppDrawer
 import com.example.telegramclone.utilities.*
-import com.google.firebase.auth.FirebaseAuth
-import com.theartofdev.edmodo.cropper.CropImage
 
 class MainActivity : AppCompatActivity() {
 
@@ -30,11 +27,17 @@ class MainActivity : AppCompatActivity() {
         APP_ACTIVITY = this
         initFirebase()
         initUser {
+            initContacts()
             initFields()
             initFunc()
         }
     }
 
+    private fun initContacts() {
+        if (checkPermission(READ_CONTACTS)) {
+            showToast("Чтение контактов")
+        }
+    }
 
 
     private fun initFunc() {
@@ -65,4 +68,17 @@ class MainActivity : AppCompatActivity() {
         super.onStop()
         AppStates.updateState(AppStates.OFFLINE)
     }
+
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (ContextCompat.checkSelfPermission(APP_ACTIVITY, READ_CONTACTS) == PackageManager.PERMISSION_GRANTED) {
+            initContacts()
+        }
+    }
+
 }
