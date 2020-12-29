b0dd410f80b02a1adbc742684dc177824bfec09b
Nikita Mager <nickita.mager@yandex.ru>
2020-12-29 16:03:31 +0700

    63.Implementation of groups. Part 3

--- app/src/main/java/com/example/telegramclone/ui/screens/groups/CreateGroupFragment.kt
+++ app/src/main/java/com/example/telegramclone/ui/screens/groups/CreateGroupFragment.kt
@@ -1,28 +1,52 @@
 package com.example.telegramclone.ui.screens.groups
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import androidx.recyclerview.widget.RecyclerView
 import com.example.telegramclone.R
+import com.example.telegramclone.database.*
 import com.example.telegramclone.models.CommonModel
 import com.example.telegramclone.ui.screens.base.BaseFragment
-import com.example.telegramclone.utilities.APP_ACTIVITY
-import com.example.telegramclone.utilities.getPlurals
-import com.example.telegramclone.utilities.hideKeyboard
-import com.example.telegramclone.utilities.showToast
+import com.example.telegramclone.ui.screens.main_list.MainListFragment
+import com.example.telegramclone.utilities.*
+import com.theartofdev.edmodo.cropper.CropImage
+import com.theartofdev.edmodo.cropper.CropImageView
 import kotlinx.android.synthetic.main.fragment_create_group.*
+import kotlinx.android.synthetic.main.fragment_settings.*
 
 class CreateGroupFragment(private var listContacts: List<CommonModel>) : BaseFragment(R.layout.fragment_create_group) {
 
     private lateinit var mRecyclerView: RecyclerView
     private lateinit var mAdapter: AddContactsAdapter
+    private var mUri = Uri.EMPTY
 
     override fun onResume() {
         super.onResume()
         APP_ACTIVITY.title = getString(R.string.create_group)
         hideKeyboard()
         initRecyclerView()
-        create_group_btn_done.setOnClickListener { showToast("Click!") }
+        create_group_photo.setOnClickListener { addPhoto() }
+        create_group_btn_done.setOnClickListener {
+            val nameGroup = create_group_input_name.text.toString()
+            if (nameGroup.isEmpty()) {
+                showToast("Введите имя группы")
+            } else {
+                createGroupToDatabase(nameGroup, mUri, listContacts) {
+                    replaceFragment(MainListFragment())
+                }
+            }
+        }
         create_group_input_name.requestFocus()
         create_group_counts.text = getPlurals(listContacts.size)
+    }
+
+    private fun addPhoto() {
+        CropImage.activity()
+            .setAspectRatio(1, 1)
+            .setRequestedSize(250, 250)
+            .setCropShape(CropImageView.CropShape.OVAL)
+            .start(APP_ACTIVITY, this)
     }
 
     private fun initRecyclerView() {
@@ -30,5 +54,16 @@ class CreateGroupFragment(private var listContacts: List<CommonModel>) : BaseFra
         mAdapter = AddContactsAdapter()
         mRecyclerView.adapter = mAdapter
         listContacts.forEach { mAdapter.updateListItems(it) }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE
+            && resultCode == Activity.RESULT_OK
+            && data != null
+        ) {
+            mUri = CropImage.getActivityResult(data).uri
+            create_group_photo.setImageURI(mUri)
+        }
     }
 }
